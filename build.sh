dead7d33f0a8a97c3f04948f6a95b8a0861b7c11
Joshua Riek <jjriek@verizon.net>
2024-04-29 09:15:38 -0400

    jammy test

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-set -x
+if [ "$(id -u)" -ne 0 ]; then 
+    echo "Please run as root"
+    exit 1
+fi
 
-cd "$(dirname -- "$(readlink -f -- "$0")")"
+cd "$(dirname -- "$(readlink -f -- "$0")")" || exit 1
 
 mkdir -p build/chroot
-cd build
+cd build || exit 1
 
 # Query the system to locate livecd-rootfs auto script installation path
 cp -r "$(dpkg -L livecd-rootfs | grep "auto$")" auto
@@ -23,6 +26,18 @@ while [ "$#" -gt 0 ]; do
             name="desktop"
             shift
             ;;
+        -j|--jammy)
+            export SUITE=jammy
+            version="22.04"
+            kernel="rockchip-5.10"
+            shift
+            ;;
+        -n|--noble)
+            export SUITE=noble
+            version="24.04"
+            kernel="rockchip"
+            shift
+            ;;
         -v|--verbose)
             set -x
             shift
@@ -38,11 +53,11 @@ while [ "$#" -gt 0 ]; do
 done
 
 export ARCH=arm64
-export SUITE=noble
 export IMAGEFORMAT=none
 export IMAGE_TARGETS=none
 export EXTRA_PPAS="jjriek/rockchip jjriek/rockchip-multimedia jjriek/panfork-mesa"
 
+# Populate the configuration directory for live build
 lb config \
 	--architecture arm64 \
 	--bootstrap-qemu-arch arm64 \
@@ -58,31 +73,48 @@ lb config \
     --mirror-binary "http://ports.ubuntu.com" \
     --parent-mirror-binary "http://ports.ubuntu.com" \
     --keyring-packages ubuntu-keyring \
-    --linux-flavours rockchip
+    --linux-flavours "${kernel}"
 
-cp -rv ../config ./
+# Add chroot tweaks and archives
+cp ../001-tweaks.chroot config/hooks/
+cp ../extra-ppas.pref.chroot config/archives/
+cp ../extra-ppas-ignore.pref.chroot config/archives/
 
 sed -i 's/libgl1-amber-dri//g' config/package-lists/livecd-rootfs.list.chroot_install
-echo "snapd/classic=stable" > config/seeded-snaps
-echo "core22/classic=stable" >> config/seeded-snaps
-echo "lxd/classic=stable" >> config/seeded-snaps
 
-echo "mali-g610-firmware" > config/package-lists/my.list.chroot
-echo "rockchip-multimedia-config" >> config/package-lists/my.list.chroot
+# Snap packages to install
+(
+    echo "snapd/classic=stable"
+    echo "core22/classic=stable"
+    echo "lxd/classic=stable"
+) > config/seeded-snaps
+
+# Generic packages to install
+(
+    echo "mali-g610-firmware"
+    echo "rockchip-multimedia-config"
+    echo "linux-firmware"
+) > config/package-lists/my.list.chroot
 
 if [ "${PROJECT}" == "ubuntu" ]; then
-    echo "ubuntu-desktop-rockchip" >> config/package-lists/my.list.chroot
-    echo "oem-config-gtk" >> config/package-lists/my.list.chroot
-    echo "ubiquity-frontend-gtk" >> config/package-lists/my.list.chroot
-    echo "ubiquity-slideshow-ubuntu" >> config/package-lists/my.list.chroot
-    echo "gstreamer1.0-rockchip1" >> config/package-lists/my.list.chroot
-    echo "chromium-browser" >> config/package-lists/my.list.chroot
-    echo "libv4l-rkmpp" >> config/package-lists/my.list.chroot
-    echo "localechooser-data" >> config/package-lists/my.list.chroot
+    # Specific packages to install for ubuntu desktop
+    (
+        echo "ubuntu-desktop-rockchip"
+        echo "oem-config-gtk"
+        echo "ubiquity-frontend-gtk"
+        echo "ubiquity-slideshow-ubuntu"
+        echo "gstreamer1.0-rockchip1"
+        echo "chromium-browser"
+        echo "libv4l-rkmpp"
+        echo "localechooser-data"
+    ) >> config/package-lists/my.list.chroot
 else
+    # Specific packages to install for ubuntu server
     echo "ubuntu-server-rockchip" >> config/package-lists/my.list.chroot
 fi
 
+# Build the rootfs
 lb build 
 
-(cd chroot/ &&  tar -p -c --sort=name --xattrs *) | xz -3 -T0 > ubuntu-24.04-preinstalled-$name-arm64.rootfs.tar.xz
+# Tar the entire rootfs
+(cd chroot/ &&  tar -p -c --sort=name --xattrs ./*) | xz -3 -T0 > "ubuntu-${version}-preinstalled-${name}-arm64.rootfs.tar.xz"
